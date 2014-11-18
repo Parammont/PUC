6819a5a670b784ca0b8f45b7cbeb7fd190acd8a4
Pedro Henrique <pedrohamc+github@gmail.com>
2014-11-18 00:40:54 -0200

    Salto Pronto

--- src/TrabalhoPratico/AnSintatico/Iniciar.java
+++ src/TrabalhoPratico/AnSintatico/Iniciar.java
@@ -6,6 +6,7 @@
  */
 package TrabalhoPratico.AnSintatico;
 
+import TrabalhoPratico.AnSemantico.Instrucoes;
 import TrabalhoPratico.Principal;
 import TrabalhoPratico.Registros.ApontadorRegistro;
 import static TrabalhoPratico.Registros.ApontadorRegistro.getApontador;
@@ -23,12 +24,12 @@ public class Iniciar {
         Principal.arquivo.Proxima();
         double i = 0;
         //do {
-            S();
+        S();
         //} while (rl != null && !(Principal.parar));
 
-            if(Principal.fimArquivo){
-                System.out.println(rl.getLinha() + ": Fim de Arquivo nao esperado ");
-            } else if (!(Principal.parar)) {
+        if (Principal.fimArquivo) {
+            System.out.println(rl.getLinha() + ": Fim de Arquivo nao esperado ");
+        } else if (!(Principal.parar)) {
             System.out.println("Compilação concluida.");
             Principal.arquivo.fecharArquivo();
         }
@@ -38,5 +39,8 @@ public class Iniciar {
         new Declaracao();
         Principal.arquivo.gravarAsm("************************** Corpo Programa ******");
         new BlocoComando();
+
+        Principal.arquivo.gravarAsm("\tHLT");
+        Principal.arquivo.gravarExe(20);
     }
 }
